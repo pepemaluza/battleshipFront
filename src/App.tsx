5d48e264eb8392535901947b2dbea0cd7bcad701
Miguel Perez <miguel.perez@ing.austral.edu.ar>
2022-06-16 09:37:46 -0300

    Code Refactor.

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Navbar from './components/navbar/Navbar'
-import LoginPage from './pages/loginPage/LoginPage'
-import HomePage from './pages/homePage/HomePage'
-
-import RLG, {WidthProvider} from 'react-grid-layout';
-
-const ReactGridLayout = WidthProvider(RLG);
+import Navbar from './components/navbar/Navbar';
+import LoginPage from './pages/loginPage/LoginPage';
+import GamePage from './pages/gamePage/GamePage';
 
 function App() {
-
-    var layoutInit: RLG.Layout[] = [
-        {i: "0", x: 0, y: 0, w: 5, h: 1},
-        {i: "1", x: 0, y: 0, w: 4, h: 1},
-        {i: "2", x: 0, y: 0, w: 3, h: 1},
-        {i: "3", x: 0, y: 0, w: 3, h: 1},
-        {i: "4", x: 0, y: 0, w: 2, h: 1},
-        {i: "5", x: 0, y: 0, w: 2, h: 1},
-        {i: "6", x: 0, y: 0, w: 2, h: 1},
-    ]
-    
-    const [layout, setLayout] = React.useState(layoutInit)
-
-    const onLayoutChange = (layout: RLG.Layout[]) => {
-        setLayout(layout);
-    }
-
-    //{i: "key", x: 0, y: 0, l: 4, v: 0}
-    const base: RLG.Layout = {i: "1", x: 0, y: 0, w: 4, h: 1};
-
-    const handleTurnShip = (n : number) => {
-        const layoutBis: RLG.Layout[] = []
-        for (let index = 0; index < layout.length; index++) {
-            if (index !== n) {
-                layoutBis.push(layout.at(index) || base);
-            } else {
-                const temp = {i: layout.at(n)?.i || "", x: layout.at(n)?.x || 0, y: layout.at(n)?.y || 0, w: layout.at(n)?.h || 0, h: layout.at(n)?.w || 0};
-                layoutBis.push(temp);
-            }
-        }
-        setLayout(layoutBis);
-    }
-
-    const handleSendBoard = () => {
-        alert(layout.toString)
-    }
-
-    const randomBoard = () => {
-        let newBoard: RLG.Layout[] = [];
-        let cond = true;
-        while (cond) {
-            for (let index = 0; index < layout.length; index++) {
-                let t = layout[index];
-                let swap = Math.floor(Math.random()*2)
-                newBoard[index] = ({i: t.i, x: Math.floor(Math.random()*(10)), y: Math.floor(Math.random()*(10)), w: swap? t.h: t.w, h: swap? t.w: t.h})
-            }
-            cond = checkOverBounds(newBoard)
-        }
-        setLayout(newBoard);
-    }
-
-    const checkOverBounds = (layout: RLG.Layout[]) => {
-        for (let index = 0; index < layout.length; index++) {
-            if (layout[index].x+layout[index].w > 10 || layout[index].y+layout[index].h > 10) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    React.useEffect(() => {
-        if (checkOverBounds(layout)) {
-            randomBoard();
-        }   
-    });
 
     return (
         <div className="App">
             <Navbar />
-            <div style={{marginLeft: 'auto', marginRight: 'auto', position: 'relative', width: '480px', marginTop: '120px', border: '1px solid black', overflow: 'hidden'}}>
-            <div style={{width: '480px', height: '480px'}}>
-                <ReactGridLayout
-                    className='layout'
-                    preventCollision={true}
-                    autoSize
-                    cols={10}
-                    rowHeight={36}
-                    compactType={null}
-                    layout={layout}
-                    maxRows={10}
-                    isBounded={true}
-                    onLayoutChange={onLayoutChange}
-                >
-                    {layout.map((lay) => {
-                        return <div key={lay.i} onDoubleClick={() => {handleTurnShip((parseInt(lay.i)) )}} />
-                    })}
-                </ReactGridLayout>
-            </div>
-            </div>
-            <button onClick={() => {handleSendBoard()}}>Enviar</button>
-            <button onClick={() => {randomBoard()}}>Aleatorio</button>
+            <GamePage />
+            <LoginPage />
         </div>
     );
 }
---
+++ src/pages/gamePage/GamePage.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import RLG, {WidthProvider} from 'react-grid-layout';
+
+const ReactGridLayout = WidthProvider(RLG);
+
+function GamePage() {
+
+    var layoutInit: RLG.Layout[] = [
+        {i: "0", x: 0, y: 0, w: 5, h: 1},
+        {i: "1", x: 0, y: 0, w: 4, h: 1},
+        {i: "2", x: 0, y: 0, w: 3, h: 1},
+        {i: "3", x: 0, y: 0, w: 3, h: 1},
+        {i: "4", x: 0, y: 0, w: 2, h: 1},
+        {i: "5", x: 0, y: 0, w: 2, h: 1},
+        {i: "6", x: 0, y: 0, w: 2, h: 1},
+    ]
+
+    const [layout, setLayout] = React.useState(layoutInit)
+
+    const onGridLayoutChange = (layout: RLG.Layout[]) => {
+        setLayout(layout);
+    }
+
+    const handleTurnShip = (n : number) => {
+        let newLayout: RLG.Layout[] = []
+        for (let i = 0; i < layout.length; i++) {
+            if (i !== n) {
+                newLayout[i] = layout[i];
+            } else {
+                newLayout[i] = {...layout[n], w: layout[n].h, h: layout[n].w};
+            }
+        }
+        setLayout(newLayout);
+    }
+
+    const handleSendBoard = () => {
+        //TODO send connect to backend.
+        console.log(layout)
+    }
+
+    const createRandomBoard = () => {
+        let newBoard: RLG.Layout[] = [];
+        let cond = true;
+        while (cond) {
+            for (let i = 0; i < layout.length; i++) {
+                let t = layout[i];
+                let swap = Math.floor(Math.random()*2)
+                let rx = Math.floor(Math.random()*(10))
+                let ry = Math.floor(Math.random()*(10))
+                newBoard[i] = ({i: t.i, x: rx, y: ry, w: swap? t.h: t.w, h: swap? t.w: t.h})
+            }
+            cond = checkOverBounds(newBoard)
+        }
+        setLayout(newBoard);
+    }
+
+    const checkOverBounds = (layout: RLG.Layout[]) => {
+        for (let index = 0; index < layout.length; index++) {
+            if (layout[index].x+layout[index].w > 10 || layout[index].y+layout[index].h > 10) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    React.useEffect(() => {
+        if (checkOverBounds(layout)) {
+            createRandomBoard();
+        }   
+    });
+
+    return (
+        <div className="game-container">
+            <div style={{marginLeft: 'auto', marginRight: 'auto', position: 'relative', width: '480px', marginTop: '120px', border: '1px solid black', overflow: 'hidden'}}>
+                <div style={{width: '480px', height: '480px'}}>
+                    <ReactGridLayout
+                        className='layout'
+                        preventCollision={true}
+                        cols={10}
+                        rowHeight={36}
+                        compactType={null}
+                        layout={layout}
+                        maxRows={10}
+                        isBounded={true}
+                        onLayoutChange={onGridLayoutChange}
+                    >
+                        {layout.map((l) => {
+                            return <div key={l.i} onDoubleClick={() => {handleTurnShip((parseInt(l.i)) )}} />
+                        })}
+                    </ReactGridLayout>
+                </div>
+            </div>
+            <button onClick={() => {createRandomBoard()}}>Aleatorio</button>
+            <button onClick={() => {handleSendBoard()}}>Enviar</button>
+        </div>
+    )
+}
+
+export default GamePage;
